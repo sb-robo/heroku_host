5be3b45f63eb4f203143ef6e453fcc7a8e902631
Suraj <reetbiswas503@gmail.com>
2021-05-09 13:44:39 +0530

    commit changed

--- app.py
+++ app.py
@@ -9,20 +9,22 @@ models, scalar = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/', methods=['POST'])
 def get_crop_recommendation():
-    json_file = request.json
-    json_data = json.load(json_file)
+    json_data = request.json
+    #print(json_file)
+    #json_data = json.load(json_file)
+    #print(json_data)
     data = []
     predictions = []
     for x in json_data:
         data.append( json_data[x])
 
-    data = scalar.transform(data.reshape(1,7))
+    data = scalar.transform(np.array(data).reshape(1,7))
     for _, model in models:
         pred = model.predict(data)
         predictions.append(pred[0])
     
     crop = stats.mode(predictions)[0][0]
-    return jsonify(crop)
+    return jsonify({"Crop Name":crop})
 
 if __name__ == '__main__':
     app.run()
