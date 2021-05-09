91773188b24924b3f151a5a9f19aa028f52d3355
Suraj <reetbiswas503@gmail.com>
2021-05-09 11:17:29 +0530

    commit changed

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import pickle
-#import numpy as np
+import numpy as np
 from scipy import stats
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 models, scalar = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/crop_recommendation', methods=['GET'])
+@app.route('/crop_recommendation', methods=['POST'])
 def get_crop_recommendation():
     json_data = request.json
     data = []
@@ -14,13 +14,13 @@ def get_crop_recommendation():
     for x in json_data:
         data.append( json_data[x])
 
-    data = scalar.transform(data)
+    data = scalar.transform(data.reshape(1,7))
     for _, model in models:
         pred = model.predict(data)
         predictions.append(pred[0])
     
-    output = stats.mode(predictions)[0][0]
-    return jsonify(output)
+    crop = stats.mode(predictions)[0][0]
+    return jsonify(crop)
 
 if __name__ == '__main__':
     app.run()
